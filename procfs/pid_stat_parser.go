8039d7434022bdb3b833eb6ff4ac0131a2840b68
Emy Parparita <emy.parparita@gmail.com>
2023-10-24 16:22:09 -0700

    wip, maybe

--- procfs/pid_stat_parser.go
+++ procfs/pid_stat_parser.go
@@ -12,16 +12,21 @@ import (
 	"strconv"
 )
 
+// The data gleaned from this file has two use cases:
+//  - as-is: the value from the file is the value associated w/ the metric, e.g. comm
+//  - for numerical calculations, e.g. utime/stime
+// As-is data will be returned such that it can be easily converted to byte
+// slices whereas numerical data will be returned as unsigned long.
+
+// Parsed data types:
+const (
+	PID_STAT_BYTES_DATA = iota
+	PID_STAT_ULONG_DATA
+)
+
 const (
 	// The max size of comm, defined in include/linux/sched.h:
 	TASK_COMM_LEN = 16
-	// The last field# scanned (note: man proc numbers fields from 1):
-	PID_STAT_LAST_SCANNED_FIELD_NUM = 41 // `(41) policy'
-	// Special handling for the fields 1..PID_STAT_LAST_SCANNED_FIELD_NUM: some
-	// should be ignored while others should be parsed as numbers. The constants
-	// below should be different than 0, which is the default action:
-	PID_STAT_IGNORE_FIELD  = 1
-	PID_STAT_NUMERIC_FIELD = 2
 	// Special TID to indicate that the stats are for PID only:
 	PID_STAT_PID_ONLY_TID = 0
 )
@@ -29,51 +34,7 @@ const (
 // Field separators, for all fields but (comm):
 var pidStatSeparators = [256]byte{' ': 1, '\t': 1, '\n': 1}
 
-// Field handling special instructions. The default action is to mark their
-// start:end boundaries and that's encoded by the default value, 0.
-var pidStatFieldHandling = [PID_STAT_LAST_SCANNED_FIELD_NUM + 1]byte{
-	// (3) state  %c
-	// (4) ppid  %d
-	// (5) pgrp  %d
-	// (6) session  %d
-	// (7) tty_nr  %d
-	// (8) tpgid  %d
-	// (9) flags  %u
-	10: PID_STAT_NUMERIC_FIELD, // (10) minflt  %lu
-	11: PID_STAT_IGNORE_FIELD,  // (11) cminflt  %lu
-	12: PID_STAT_NUMERIC_FIELD, // (12) majflt  %lu
-	13: PID_STAT_IGNORE_FIELD,  // (13) cmajflt  %lu
-	14: PID_STAT_NUMERIC_FIELD, // (14) utime  %lu
-	15: PID_STAT_NUMERIC_FIELD, // (15) stime  %lu
-	16: PID_STAT_IGNORE_FIELD,  // (16) cutime  %ld
-	17: PID_STAT_IGNORE_FIELD,  // (17) cstime  %ld
-	// (18) priority  %ld
-	// (19) nice  %ld
-	// (20) num_threads  %ld
-	21: PID_STAT_IGNORE_FIELD, // (21) itrealvalue  %ld
-	// (22) starttime  %llu
-	// (23) vsize  %lu
-	// (24) rss  %ld
-	// (25) rsslim  %lu
-	26: PID_STAT_IGNORE_FIELD, // (26) startcode  %lu  [PT]
-	27: PID_STAT_IGNORE_FIELD, // (27) endcode  %lu  [PT]
-	28: PID_STAT_IGNORE_FIELD, // (28) startstack  %lu  [PT]
-	29: PID_STAT_IGNORE_FIELD, // (29) kstkesp  %lu  [PT]
-	30: PID_STAT_IGNORE_FIELD, // (30) kstkeip  %lu  [PT]
-	31: PID_STAT_IGNORE_FIELD, // (31) signal  %lu
-	32: PID_STAT_IGNORE_FIELD, // (32) blocked  %lu
-	33: PID_STAT_IGNORE_FIELD, // (33) sigignore  %lu
-	34: PID_STAT_IGNORE_FIELD, // (34) sigcatch  %lu
-	35: PID_STAT_IGNORE_FIELD, // (35) wchan  %lu  [PT]
-	36: PID_STAT_IGNORE_FIELD, // (36) nswap  %lu
-	37: PID_STAT_IGNORE_FIELD, // (37) cnswap  %lu
-	38: PID_STAT_IGNORE_FIELD, // (38) exit_signal  %d  (since Linux 2.1.22)
-	// (39) processor  %d  (since Linux 2.2.8)
-	// (40) rt_priority  %u  (since Linux 2.5.19)
-	// (41) policy  %u  (since Linux 2.5.19)
-}
-
-// The following enumeration gives the indices for fields kept after the scan:
+// The following enumeration gives the indices for byte slice fields:
 const (
 	PID_STAT_COMM = iota
 	PID_STAT_STATE
@@ -97,60 +58,124 @@ const (
 	PID_STAT_BYTE_SLICE_FIELD_COUNT // Must be last!
 )
 
+// The field# to use for byte slice fields (as per man proc field# starts from 1):
+var pidStatByteFieldNum = [PID_STAT_BYTE_SLICE_FIELD_COUNT]int{
+	PID_STAT_COMM:        2,
+	PID_STAT_STATE:       3,
+	PID_STAT_PPID:        4,
+	PID_STAT_PGRP:        5,
+	PID_STAT_SESSION:     6,
+	PID_STAT_TTY_NR:      7,
+	PID_STAT_TPGID:       8,
+	PID_STAT_FLAGS:       9,
+	PID_STAT_PRIORITY:    18,
+	PID_STAT_NICE:        19,
+	PID_STAT_NUM_THREADS: 20,
+	PID_STAT_STARTTIME:   22,
+	PID_STAT_VSIZE:       23,
+	PID_STAT_RSS:         24,
+	PID_STAT_RSSLIM:      25,
+	PID_STAT_PROCESSOR:   39,
+	PID_STAT_RT_PRIORITY: 40,
+	PID_STAT_POLICY:      41,
+}
+
+// The following enumeration gives the indices for unsigned long fields:
 const (
 	PID_STAT_MINFLT = iota
 	PID_STAT_MAJLT
 	PID_STAT_UTIME
 	PID_STAT_STIME
 
-	PID_STAT_NUMERIC_FIELD_COUNT // Must be last!
+	PID_STAT_ULONG_DATA_COUNT // Must be last!
 )
 
-type PidStatByteFields struct {
+// The field# to use for unsigned long fields (as per man proc field# starts from 1):
+var pidStatUlongFieldNum = [PID_STAT_ULONG_DATA_COUNT]int{
+	PID_STAT_MINFLT: 10,
+	PID_STAT_MAJLT:  12,
+	PID_STAT_UTIME:  14,
+	PID_STAT_STIME:  15,
+}
+
+// Field handling:
+type PidStatFieldHandling struct {
+	// How to parse the field:
+	dataType byte
+	// Array index where to store the result:
+	index int
+}
+
+// The following list maps field# into its handling; it will be built during
+// init based on the ...FieldNum above; nil indicates that the field should be
+// ignored:
+var pidStatFieldNumHandling []*PidStatFieldHandling
+
+func init() {
+	maxFieldNum := 0
+	for _, fieldNum := range pidStatByteFieldNum {
+		if fieldNum > maxFieldNum {
+			maxFieldNum = fieldNum
+		}
+	}
+	for _, fieldNum := range pidStatUlongFieldNum {
+		if fieldNum > maxFieldNum {
+			maxFieldNum = fieldNum
+		}
+	}
+
+	pidStatFieldNumHandling = make([]*PidStatFieldHandling, maxFieldNum+1)
+	for index, fieldNum := range pidStatByteFieldNum {
+		pidStatFieldNumHandling[fieldNum] = &PidStatFieldHandling{PID_STAT_BYTES_DATA, index}
+	}
+	for index, fieldNum := range pidStatUlongFieldNum {
+		pidStatFieldNumHandling[fieldNum] = &PidStatFieldHandling{PID_STAT_ULONG_DATA, index}
+	}
+}
+
+type PidStat struct {
 	// Read the raw content of the file here:
 	Buf *bytes.Buffer
-	// Start/stop for each byte slice field:
-	FieldStart, FieldEnd [PID_STAT_BYTE_SLICE_FIELD_COUNT]int
+	// Start/stop index for each byte slice field:
+	FieldStart, FieldEnd []int
 	// Numeric fields:
-	NumericFields [PID_STAT_NUMERIC_FIELD_COUNT]uint64
-	// The path file to read; if empty then the raw content is assumed loaded
-	// already:
-	filePath string
+	NumericFields []uint64
+	// The path file to read:
+	path string
 }
 
-func (bf *PidStatByteFields) SetPath(procfsRoot string, pid, tid int) {
+func NewPidStat(procfsRoot string, pid, tid int) *PidStat {
+	pidStat := &PidStat{
+		Buf:           &bytes.Buffer{},
+		FieldStart:    make([]int, PID_STAT_BYTE_SLICE_FIELD_COUNT),
+		FieldEnd:      make([]int, PID_STAT_BYTE_SLICE_FIELD_COUNT),
+		NumericFields: make([]uint64, PID_STAT_ULONG_DATA_COUNT),
+	}
 	if tid == PID_STAT_PID_ONLY_TID {
-		bf.filePath = path.Join(procfsRoot, strconv.Itoa(pid), "stat")
+		pidStat.path = path.Join(procfsRoot, strconv.Itoa(pid), "stat")
 	} else {
-		bf.filePath = path.Join(procfsRoot, strconv.Itoa(pid), "task", strconv.Itoa(tid), "stat")
+		pidStat.path = path.Join(procfsRoot, strconv.Itoa(pid), "task", strconv.Itoa(tid), "stat")
 	}
+	return pidStat
 }
 
-func (bf *PidStatByteFields) ClearPath() {
-	bf.filePath = ""
-}
-
-// Read file if .filePath is set and parse content.
-func (bf *PidStatByteFields) Parse() error {
-	var err error
-
-	if bf.filePath != "" {
-		file, err := os.Open(bf.filePath)
-		if err != nil {
-			return err
-		}
-		_, err = bf.Buf.ReadFrom(file)
-		if err != nil {
-			return err
-		}
+// Read file if .path is set and parse content.
+func (pidStat *PidStat) Parse() error {
+	file, err := os.Open(pidStat.path)
+	if err != nil {
+		return err
 	}
-	b := bf.Buf.Bytes()
-	l := bf.Buf.Len()
+	_, err = pidStat.Buf.ReadFrom(file)
+	if err != nil {
+		return err
+	}
+	b := pidStat.Buf.Bytes()
+	l := pidStat.Buf.Len()
 
 	// Locate '(' for comm start:
 	commStart := bytes.IndexByte(b, '(') + 1
 	if commStart <= 0 {
-		return fmt.Errorf("%s: cannot locate '('", bf.filePath)
+		return fmt.Errorf("%s: cannot locate '('", pidStat.path)
 	}
 	// Locate ')' for comm end, it should be at most TASK_COMM_LEN after
 	// commStart:
@@ -165,54 +190,39 @@ func (bf *PidStatByteFields) Parse() error {
 		// TASK_COMM_LEN? Try again against the whole buffer:
 		commEnd = bytes.LastIndexByte(b, ')')
 		if commEnd < 0 {
-
-			return fmt.Errorf("%s: cannot locate ')'", bf.filePath)
+			return fmt.Errorf("%s: cannot locate ')'", pidStat.path)
 		}
 	}
 
-	byteSliceFieldNum := 0
+	pidStat.FieldStart[PID_STAT_COMM] = commStart
+	pidStat.FieldEnd[PID_STAT_COMM] = commEnd
 
-	bf.FieldStart[byteSliceFieldNum] = commStart
-	bf.FieldEnd[byteSliceFieldNum] = commEnd
-	byteSliceFieldNum++
+	fieldNum := pidStatByteFieldNum[PID_STAT_COMM]
+	maxFieldNum := len(pidStatFieldNumHandling) - 1
 
-	numericFieldNum := 0
-	fieldStart := 0
-	for wasSep, scanComplete, i, scannedFieldNum := byte(1), false, commEnd+1, 2; !scanComplete && i < l; i++ {
+	for wasSep, i, fieldStart := byte(1), commEnd+1, commEnd+1; i < l && fieldNum < maxFieldNum; i++ {
 		isSep := pidStatSeparators[b[i]]
 		switch wasSep<<1 + isSep {
 		case 0b10:
-			scannedFieldNum++
 			fieldStart = i
 		case 0b01:
-			switch pidStatFieldHandling[scannedFieldNum] {
-			case PID_STAT_IGNORE_FIELD:
-				break
-			case PID_STAT_NUMERIC_FIELD:
-				bf.NumericFields[numericFieldNum], err = strconv.ParseUint(string(b[fieldStart:i]), 10, 64)
-				if err != nil {
-					return fmt.Errorf("%s: field# %d: %v", bf.filePath, scannedFieldNum, err)
+			fieldNum++
+			fieldHandling := pidStatFieldNumHandling[fieldNum]
+			if fieldHandling != nil {
+				index := fieldHandling.index
+				switch fieldHandling.dataType {
+				case PID_STAT_BYTES_DATA:
+					pidStat.FieldStart[index] = fieldStart
+					pidStat.FieldEnd[index] = i
+				case PID_STAT_ULONG_DATA:
+					pidStat.NumericFields[index], err = strconv.ParseUint(string(b[fieldStart:i]), 10, 64)
+					if err != nil {
+						return fmt.Errorf("%s: field# %d: %v", pidStat.path, fieldNum, err)
+					}
 				}
-				numericFieldNum++
-			default:
-				bf.FieldStart[byteSliceFieldNum] = fieldStart
-				bf.FieldEnd[byteSliceFieldNum] = i
-				byteSliceFieldNum++
 			}
-			scanComplete = scannedFieldNum == PID_STAT_LAST_SCANNED_FIELD_NUM
 		}
 		wasSep = isSep
 	}
-
-	// Sanity check:
-	if byteSliceFieldNum != PID_STAT_BYTE_SLICE_FIELD_COUNT || numericFieldNum != PID_STAT_NUMERIC_FIELD_COUNT {
-		return fmt.Errorf(
-			"%s: scan incomplete: byte slice got/want field#: %d/%d, numeric got/want field#: %d/%d",
-			bf.filePath,
-			byteSliceFieldNum, PID_STAT_BYTE_SLICE_FIELD_COUNT,
-			numericFieldNum, PID_STAT_NUMERIC_FIELD_COUNT,
-		)
-	}
-
 	return nil
 }
