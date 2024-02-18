7e5853f2c5def7e0dcb0047d29dddb0348275336
Emy Parparita <emy.parparita@gmail.com>
2024-02-17 16:20:49 -0800

    scheduler wip

--- lsvmi/scheduler.go
+++ lsvmi/scheduler.go
@@ -31,9 +31,9 @@ package lsvmi
 //        +------------+ +------------+
 //              ^               |
 //     new task |               |
-//                    +---------+---- ... ----+
-//                    | task    | task        | task
-//                    v         v             v
+//                   +----------+--- ... ----+
+//                   | task     | task       | task
+//                   v          v            v
 //              +--------+ +--------+   +--------+
 //              | Worker | | Worker |...| Worker |
 //              +--------+ +--------+   +--------+
@@ -45,11 +45,11 @@ package lsvmi
 // sorted by the task's deadline (i.e. the nearest deadline is at the top).
 //
 // The Dispatcher maintains a timer for the next deadline based on the top of
-// the heap and it also monitors the Task Queue for new additions. Whichever
-// comes first, it determines the next task to run. The latter's deadlines is
-// updated and, if a new task, it is also added to heap; if not then the heap is
-// updated in place. The task is marked as scheduled and it is placed into the
-// TODO Queue.
+// the heap and it also monitors the Task Queue for new additions, whichever
+// comes first. Based on those, it selects the next task to run. The latter's
+// deadlines is updated and, if a new task, it is also added to heap; if not
+// then the heap is updated in place. The task is marked as scheduled and it is
+// placed into the TODO Queue.
 //
 // The TODO Queue feeds the Worker Pool; the number of workers in the pool
 // controls the level of concurrency of task execution and it allows for short
@@ -78,11 +78,37 @@ const (
 	SCHEDULER_STATE_STOPPED
 )
 
+const (
+	// Indexes into Scheduler.stats.[id].uint64Stats
+
+	// How many times the task was scheduled, indexed by Task.id:
+	TASK_STATS_SCHEDULED_COUNT = iota
+
+	// How many time the task was not queued for execution because it was
+	// pending a previously scheduled one, indexed by Task.id:
+	TASK_STATS_OVER_SCHEDULED_COUNT
+
+	// Must be last:
+	TASK_STATS_UINT64_LEN
+)
+
+const (
+	// Indexes into Scheduler.stats.[id].float64Stats
+
+	// Total run time, in seconds:
+	TASK_STATS_RUNTIME_SEC = iota
+
+	// Must be last:
+	TASK_STATS_FLOAT64_LEN
+)
+
 type TaskAction interface {
 	Execute()
 }
 
 type Task struct {
+	// Id, used for stats:
+	id string
 	// Deadline:
 	deadline time.Time
 	// Interval:
@@ -93,6 +119,13 @@ type Task struct {
 	scheduled bool
 }
 
+type TaskStats struct {
+	uint64Stats  []uint64
+	float64Stats []float64
+}
+
+type SchedulerStats map[string]*TaskStats
+
 type Scheduler struct {
 	// Next Task Heap:
 	tasks []*Task
@@ -102,6 +135,8 @@ type Scheduler struct {
 	numWorkers int
 	// The state of the scheduler, whether it is running or not:
 	state SchedulerState
+	// Stats:
+	stats SchedulerStats
 	// General purpose lock for atomic operations: check task `scheduled` flag,
 	// scheduler's `state`, etc. The lock is shared because the contention is
 	// minimal, it doesn't make sense to use individual lock.
@@ -124,10 +159,18 @@ func (s SchedulerState) String() string {
 
 var schedulerLog = NewCompLogger("scheduler")
 
-func NewTask(interval time.Duration, action TaskAction) *Task {
+func NewTask(id string, interval time.Duration, action TaskAction) *Task {
 	return &Task{
+		id:       id,
 		interval: interval,
 		action:   action,
+	}
+}
+
+func NewTaskStats() *TaskStats {
+	return &TaskStats{
+		uint64Stats:  make([]uint64, TASK_STATS_UINT64_LEN),
+		float64Stats: make([]float64, TASK_STATS_FLOAT64_LEN),
 	}
 }
 
@@ -137,6 +180,7 @@ func NewScheduler(numWorkers int) *Scheduler {
 		taskQ:      make(chan *Task, SCHEDULER_TASK_Q_LEN),
 		todoQ:      make(chan *Task, SCHEDULER_TODO_Q_LEN),
 		numWorkers: numWorkers,
+		stats:      make(SchedulerStats),
 		state:      SCHEDULER_STATE_CREATED,
 		mu:         &sync.Mutex{},
 		wg:         &sync.WaitGroup{},
@@ -239,6 +283,17 @@ func (scheduler *Scheduler) dispatcherLoop() {
 			if queueIt {
 				task.scheduled = true
 			}
+			if scheduler.stats != nil {
+				taskStats := scheduler.stats[task.id]
+				if taskStats == nil {
+					taskStats = NewTaskStats()
+					scheduler.stats[task.id] = taskStats
+				}
+				taskStats.uint64Stats[TASK_STATS_SCHEDULED_COUNT] += 1
+				if !queueIt {
+					taskStats.uint64Stats[TASK_STATS_OVER_SCHEDULED_COUNT] += 1
+				}
+			}
 			scheduler.mu.Unlock()
 			if queueIt {
 				scheduler.todoQ <- task
@@ -260,24 +315,53 @@ func (scheduler *Scheduler) workerLoop(workerId int) {
 		if !isOpen {
 			return
 		}
+		startTs := time.Now()
 		if task.action != nil {
 			task.action.Execute()
 		}
+		taskRuntime := time.Since(startTs).Seconds()
 		scheduler.mu.Lock()
+		scheduler.stats[task.id].float64Stats[TASK_STATS_RUNTIME_SEC] += taskRuntime
 		task.scheduled = false
 		scheduler.mu.Unlock()
 	}
 }
 
+func (scheduler *Scheduler) SnapStats(to SchedulerStats) SchedulerStats {
+	if scheduler.stats == nil {
+		return nil
+	}
+	if to == nil {
+		to = make(SchedulerStats)
+	}
+	scheduler.mu.Lock()
+	for taskId, taskStats := range scheduler.stats {
+		toTaskStats := to[taskId]
+		if toTaskStats == nil {
+			toTaskStats = NewTaskStats()
+			to[taskId] = toTaskStats
+		}
+		copy(toTaskStats.uint64Stats, taskStats.uint64Stats)
+		copy(toTaskStats.float64Stats, taskStats.float64Stats)
+	}
+	scheduler.mu.Unlock()
+	return to
+}
+
 func (scheduler *Scheduler) Start() {
 	scheduler.mu.Lock()
-	defer scheduler.mu.Unlock()
+	entryState := scheduler.state
+	canStart := entryState == SCHEDULER_STATE_CREATED
+	if canStart {
+		scheduler.state = SCHEDULER_STATE_RUNNING
+	}
+	scheduler.mu.Unlock()
 
-	if scheduler.state != SCHEDULER_STATE_CREATED {
+	if !canStart {
 		schedulerLog.Warnf(
 			"scheduler can only be started from state %d '%s', not from %d '%s'",
 			SCHEDULER_STATE_CREATED, SchedulerState(SCHEDULER_STATE_CREATED),
-			scheduler.state, scheduler.state,
+			entryState, entryState,
 		)
 		return
 	}
@@ -292,26 +376,24 @@ func (scheduler *Scheduler) Start() {
 		go scheduler.workerLoop(workerId)
 	}
 
-	scheduler.state = SCHEDULER_STATE_RUNNING
 	schedulerLog.Info("scheduler started")
 }
 
 func (scheduler *Scheduler) Shutdown() {
 	scheduler.mu.Lock()
-	defer scheduler.mu.Unlock()
+	stopped := scheduler.state == SCHEDULER_STATE_STOPPED
+	scheduler.state = SCHEDULER_STATE_STOPPED
+	scheduler.mu.Unlock()
 
-	if scheduler.state == SCHEDULER_STATE_STOPPED {
-		schedulerLog.Warnf(
-			"scheduler already in state %d '%s'",
-			scheduler.state, scheduler.state,
-		)
+	if stopped {
+		schedulerLog.Warn("scheduler already stopped")
 		return
 	}
 
 	schedulerLog.Info("stop scheduler")
+
 	close(scheduler.taskQ)
 	scheduler.wg.Wait()
 
-	scheduler.state = SCHEDULER_STATE_STOPPED
 	schedulerLog.Info("scheduler stopped")
 }
--- lsvmi/scheduler_test.go
+++ lsvmi/scheduler_test.go
@@ -3,6 +3,8 @@
 package lsvmi
 
 import (
+	"bytes"
+	"fmt"
 	"strconv"
 	"testing"
 	"time"
@@ -11,12 +13,17 @@ import (
 )
 
 type SchedulerExecuteTestCase struct {
+	numWorkers  int
 	intervals   []time.Duration
 	runDuration time.Duration
+	// The scheduled intervals will be checked against the desired one and they
+	// should be in the range of:
+	//  (1 - scheduleIntervalPct/100)*interval .. (1 + scheduleIntervalPct/100)*interval
+	// Set to 0 to disable:
+	scheduleIntervalPct float64
 }
 
 type TestSchedulerTaskAction struct {
-	taskId     string
 	task       *Task
 	timestamps []time.Time
 }
@@ -25,46 +32,108 @@ func (action *TestSchedulerTaskAction) Execute() {
 	action.timestamps = append(action.timestamps, time.Now())
 	schedulerLog.Infof(
 		"Execute task %s: interval: %s, next deadline: %s",
-		action.taskId, action.task.interval, action.task.deadline.Format(time.RFC3339Nano),
+		action.task.id, action.task.interval, action.task.deadline.Format(time.RFC3339Nano),
 	)
+	//time.Sleep(2 * action.task.interval) // force over-scheduling
 }
 
 func testSchedulerExecute(tc *SchedulerExecuteTestCase, t *testing.T) {
 	tlc := testutils.NewTestingLogCollect(t, Log, nil)
 	defer tlc.RestoreLog()
 
-	scheduler := NewScheduler(4)
+	numWorkers := tc.numWorkers
+	if numWorkers <= 0 {
+		numWorkers = 1
+	}
+	scheduler := NewScheduler(numWorkers)
 	scheduler.Start()
 
+	tasks := make([]*Task, len(tc.intervals))
 	for i, interval := range tc.intervals {
-		action := &TestSchedulerTaskAction{
-			taskId: strconv.Itoa(i),
-		}
-		task := NewTask(interval, action)
+		action := &TestSchedulerTaskAction{}
+		task := NewTask(strconv.Itoa(i), interval, action)
 		action.task = task
 		scheduler.AddTask(task)
+		tasks[i] = task
 	}
 
 	time.Sleep(tc.runDuration)
 	scheduler.Shutdown()
 
+	// Verify that each task was scheduled roughly at the expected intervals and
+	// that it wasn't skipped:
+
+	errBuf := &bytes.Buffer{}
+
+	stats := scheduler.SnapStats(nil)
+	for _, task := range tasks {
+		taskStats := stats[task.id]
+		if taskStats == nil {
+			fmt.Fprintf(errBuf, "\n task %s: missing stats", task.id)
+			continue
+		} else if taskStats.uint64Stats[TASK_STATS_OVER_SCHEDULED_COUNT] > 0 {
+			fmt.Fprintf(
+				errBuf, "\n task %s: over/scheduled %d/%d time(s)",
+				task.id,
+				taskStats.uint64Stats[TASK_STATS_OVER_SCHEDULED_COUNT],
+				taskStats.uint64Stats[TASK_STATS_SCHEDULED_COUNT],
+			)
+			continue
+		}
+		if tc.scheduleIntervalPct <= 0 || tc.scheduleIntervalPct > 100 {
+			continue
+		}
+		pct := tc.scheduleIntervalPct / 100.
+		intervalSec := task.interval.Seconds()
+		minIntervalSec := (1 - pct) * intervalSec
+		maxIntervalSec := (1 + pct) * intervalSec
+
+		timestamps := task.action.(*TestSchedulerTaskAction).timestamps
+		// timestamp#0 -> #1 may be irregular, but everything #(k-1) -> #k, k >=
+		// 2, should be checked:
+		for k := 2; k < len(timestamps); k++ {
+			gotIntervalSec := timestamps[k].Sub(timestamps[k-1]).Seconds()
+			if gotIntervalSec < minIntervalSec || maxIntervalSec < gotIntervalSec {
+				fmt.Fprintf(
+					errBuf,
+					"\ntask %s interval# %d: want: %.06f..%.06f, got: %.06f sec",
+					task.id, k, minIntervalSec, maxIntervalSec, gotIntervalSec,
+				)
+			}
+		}
+	}
+	if errBuf.Len() > 0 {
+		tlc.Fatal(errBuf)
+	}
+
 }
 
 func TestSchedulerExecute(t *testing.T) {
 	timeUnit := 100 * time.Millisecond
+	scheduleIntervalPct := 10.
 
 	for _, tc := range []*SchedulerExecuteTestCase{
 		{
 			intervals: []time.Duration{
 				1 * timeUnit,
 			},
-			runDuration: 10 * timeUnit,
+			runDuration:         10 * timeUnit,
+			scheduleIntervalPct: scheduleIntervalPct,
 		},
 		{
 			intervals: []time.Duration{
-				7 * timeUnit, 3 * timeUnit, 5 * timeUnit, 1 * timeUnit,
+				4 * timeUnit, 7 * timeUnit, 3 * timeUnit, 5 * timeUnit, 1 * timeUnit,
 			},
-			runDuration: 43 * timeUnit,
+			runDuration:         43 * timeUnit,
+			scheduleIntervalPct: scheduleIntervalPct,
+		},
+		{
+			numWorkers: 4,
+			intervals: []time.Duration{
+				4 * timeUnit, 7 * timeUnit, 3 * timeUnit, 5 * timeUnit, 1 * timeUnit,
+			},
+			runDuration:         43 * timeUnit,
+			scheduleIntervalPct: scheduleIntervalPct,
 		},
 	} {
 		t.Run(
