9d8a42f4aac37867df0f1e27ab56ad4050f76102
Emy Parparita <emy.parparita@gmail.com>
2024-03-04 16:52:28 -0800

    cleanup

--- testutils/common.go
+++
@@ -1,9 +0,0 @@
-package testutils
-
-// The following sub-dirs are relative to module root:
-const (
-	TESTDATA_SUBDIR        = "testdata"
-	TESTDATA_PROC_SUBDIR   = TESTDATA_SUBDIR + "/proc"
-	TESTDATA_PROCFS_SUBDIR = TESTDATA_SUBDIR + "/procfs"
-	TESTCASES_SUBDIR       = TESTDATA_SUBDIR + "/testcases"
-)
