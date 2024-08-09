7eb1b7bca6db23814dd146be00681915c40e359c
Emy Parparita <emy.parparita@gmail.com>
2024-08-08 17:21:31 -0700

    proc pid stat metrics fmt test completed

--- lsvmi/proc_pid_metrics_test.go
+++ lsvmi/proc_pid_metrics_test.go
@@ -15,13 +15,11 @@ type TestProcPidMetricsFmtTestCase struct {
 	wantMetric string
 }
 
-func TestProcPidMetricsInitMetricsCache(t *testing.T) {
+func TestProcPidMetricsInitPidStatMetricsCache(t *testing.T) {
 	const (
 		TEST_INSTANCE       = "INSTANCE"
 		TEST_HOSTNAME       = "HOST"
-		TEST_PID            = 100
-		TEST_TID            = 101
-		TEST_PID_TID_LABELS = `pid="100",tid="101"`
+		TEST_PID_TID_LABELS = `pid="PID",tid="TID"`
 		TEST_STARTTIME      = "STARTTIME_MS"
 		TEST_TIMESTAMP      = "TIMESTAMP"
 	)
@@ -43,22 +41,65 @@ func TestProcPidMetricsInitMetricsCache(t *testing.T) {
 		{
 			pm.pidStatStateMetricFmt,
 			[]any{TEST_PID_TID_LABELS, TEST_STARTTIME, []byte("S"), '1', TEST_TIMESTAMP},
-			`proc_pid_stat_state{instance="INSTANCE",hostname="HOST",pid="100",tid="101",starttime_msec="STARTTIME_MS",state="S"} 1 TIMESTAMP` + "\n",
+			`proc_pid_stat_state{instance="INSTANCE",hostname="HOST",pid="PID",tid="TID",starttime_msec="STARTTIME_MS",state="S"} 1 TIMESTAMP`,
 		},
 		{
 			pm.pidStatInfoMetricFmt,
 			[]any{TEST_PID_TID_LABELS, []byte("COMM"), []byte("PPID"), []byte("PGRP"), []byte("SESSION"), []byte("TTY_NR"), []byte("TPGID"), []byte("FLAGS"), []byte("PRIORITY"), []byte("NICE"), []byte("RT_PRIORITY"), []byte("POLICY"), '1', TEST_TIMESTAMP},
-			`proc_pid_stat_info{instance="INSTANCE",hostname="HOST",pid="100",tid="101",comm="COMM",ppid="PPID",pgrp="PGRP",session="SESSION",tty="TTY_NR",tpgid="TPGID",flags="FLAGS",prio="PRIORITY",nice="NICE",rt_prio="RT_PRIORITY",policy="POLICY"} 1 TIMESTAMP` + "\n",
+			`proc_pid_stat_info{instance="INSTANCE",hostname="HOST",pid="PID",tid="TID",comm="COMM",ppid="PPID",pgrp="PGRP",session="SESSION",tty="TTY_NR",tpgid="TPGID",flags="FLAGS",prio="PRIORITY",nice="NICE",rt_prio="RT_PRIORITY",policy="POLICY"} 1 TIMESTAMP`,
 		},
 		{
 			pm.pidStatCpuNumMetricFmt,
 			[]any{TEST_PID_TID_LABELS, []byte("CPU_N"), TEST_TIMESTAMP},
-			`proc_pid_stat_cpu_num{instance="INSTANCE",hostname="HOST",pid="100",tid="101"} CPU_N TIMESTAMP` + "\n",
+			`proc_pid_stat_cpu_num{instance="INSTANCE",hostname="HOST",pid="PID",tid="TID"} CPU_N TIMESTAMP`,
+		},
+		{
+			pm.pidStatMemoryMetricFmt[0].fmt,
+			[]any{TEST_PID_TID_LABELS, []byte("VSIZE"), TEST_TIMESTAMP},
+			`proc_pid_stat_vsize{instance="INSTANCE",hostname="HOST",pid="PID",tid="TID"} VSIZE TIMESTAMP`,
+		},
+		{
+			pm.pidStatMemoryMetricFmt[1].fmt,
+			[]any{TEST_PID_TID_LABELS, []byte("RSS"), TEST_TIMESTAMP},
+			`proc_pid_stat_rss{instance="INSTANCE",hostname="HOST",pid="PID",tid="TID"} RSS TIMESTAMP`,
+		},
+		{
+			pm.pidStatMemoryMetricFmt[2].fmt,
+			[]any{TEST_PID_TID_LABELS, []byte("RSSLIM"), TEST_TIMESTAMP},
+			`proc_pid_stat_rsslim{instance="INSTANCE",hostname="HOST",pid="PID",tid="TID"} RSSLIM TIMESTAMP`,
+		},
+		{
+			pm.pidStatPcpuMetricFmt[0].fmt,
+			[]any{TEST_PID_TID_LABELS, 0.123454600, TEST_TIMESTAMP},
+			`proc_pid_stat_pcpu{instance="INSTANCE",hostname="HOST",pid="PID",tid="TID"} 0.1 TIMESTAMP`,
+		},
+		{
+			pm.pidStatPcpuMetricFmt[1].fmt,
+			[]any{TEST_PID_TID_LABELS, 1.123454600, TEST_TIMESTAMP},
+			`proc_pid_stat_stime_pcpu{instance="INSTANCE",hostname="HOST",pid="PID",tid="TID"} 1.1 TIMESTAMP`,
+		},
+		{
+			pm.pidStatPcpuMetricFmt[2].fmt,
+			[]any{TEST_PID_TID_LABELS, 2.123454600, TEST_TIMESTAMP},
+			`proc_pid_stat_utime_pcpu{instance="INSTANCE",hostname="HOST",pid="PID",tid="TID"} 2.1 TIMESTAMP`,
+		},
+		{
+			pm.pidStatFltMetricFmt[0].fmt,
+			[]any{TEST_PID_TID_LABELS, 100, TEST_TIMESTAMP},
+			`proc_pid_stat_minflt_delta{instance="INSTANCE",hostname="HOST",pid="PID",tid="TID"} 100 TIMESTAMP`,
+		},
+		{
+			pm.pidStatFltMetricFmt[1].fmt,
+			[]any{TEST_PID_TID_LABELS, 200, TEST_TIMESTAMP},
+			`proc_pid_stat_majflt_delta{instance="INSTANCE",hostname="HOST",pid="PID",tid="TID"} 200 TIMESTAMP`,
 		},
 	} {
+		wantMetric := tc.wantMetric + "\n"
 		gotMetric := fmt.Sprintf(tc.fmt, tc.args...)
-		if tc.wantMetric != gotMetric {
-			t.Errorf("\n\twant: %q\n\t got: %q", tc.wantMetric, gotMetric)
+		if wantMetric != gotMetric {
+			t.Errorf("\n\twant: %q\n\t got: %q", wantMetric, gotMetric)
+		} else {
+			t.Log(tc.wantMetric)
 		}
 	}
 
